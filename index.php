c88667bdf76e180157b8580e564c8e3b54c2529a
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-24 18:19:09 +0100

    add session_start

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>Password Generator</title>
 	<!-- Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
 	L'esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato. -->
 </head>
@@ -14,13 +14,25 @@
 		<input type="number" name="length" min="10" max="100">
         <button type="submit">GENERA PASSWORD</button>
     </form>
+	<br>
 
 	<?php 
-		include __DIR__ . "/functions.php"
+
+    session_start();
+	
 	?>
+	<br><br>
+	<?php
+	include __DIR__ . "/functions.php"
+	?>
+
+	
 
 </body>
 </html>
+
+
+
 
 
 
